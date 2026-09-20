ccd6cb0678d53f0dd7a9de02e15591cc6dbc658e
mriyer27 <mriyer@syr.edu>
2026-09-20 22:44:37 +0000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="HW Manager", page_icon="🏠", layout="wide")
 hw1_page = st.Page("HW1.py", title="HW1: Document Q&A", icon="📄")
 hw2_page = st.Page("HW2.py", title="HW2: URL Summarizer", icon="🌐")
 hw3_page = st.Page("HW3.py", title="HW3: URL Chatbot", icon="💬")
-hw4_page = st.Page("HW4.py", title="HW4: Org Chatbot (RAG)", icon="🎓")
+hw4_page = st.Page("HW4.py", title="HW4: Org Chatbot (RAG)", icon="🎓", default = True)
 
 pg = st.navigation([hw1_page, hw2_page, hw3_page, hw4_page])
 pg.run()
